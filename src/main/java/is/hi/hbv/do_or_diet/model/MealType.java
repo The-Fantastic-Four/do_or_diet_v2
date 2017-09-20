706bd5b479a23228869300f0c9d7fc5c9a1148ab
Viktor Alex <viktoralex@365.is>
2017-09-20 17:04:00 +0000

    Enhanced meal type enumerator

--- src/main/java/is/hi/hbv/do_or_diet/model/MealType.java
+++ src/main/java/is/hi/hbv/do_or_diet/model/MealType.java
@@ -1,7 +1,26 @@
 package is.hi.hbv.do_or_diet.model;
 
 public enum MealType {
-	BREAKFAST, 
-	LUNCH, 
-	DINNER
+	BREAKFAST("Morgunmatur", "breakfast"), 
+	LUNCH("Hádegismatur", "lunch"), 
+	DINNER("Kvöldmatur", "dinner");
+	
+	private final String displayName;
+	private final String shortName;
+	
+	private MealType(String displayName, String shortName)
+	{
+		this.displayName = displayName;
+		this.shortName = shortName;
+	}
+	
+	public String getDisplayName()
+	{
+		return this.displayName;
+	}
+	
+	public String getShortName()
+	{
+		return this.shortName;
+	}
 }
