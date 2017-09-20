419e7e27eb50e83514a5a28d13453eb1fcff1c24
Ragnheiður Ásta Karlsdóttir <rak4@hi.is>
2017-09-20 22:24:20 +0000

    Er að prófa að commita röngu til að skoða hvernig conflictið kemur út.

--- src/main/java/is/hi/hbv/do_or_diet/repository/RecipeRepository.java
+++ src/main/java/is/hi/hbv/do_or_diet/repository/RecipeRepository.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Repository;
 
 import is.hi.hbv.do_or_diet.model.Recipe;
 
+/*
+ * Er að prófa að bæta við röngu kommenti.
+ */
 @Repository
 public class RecipeRepository 
 {
