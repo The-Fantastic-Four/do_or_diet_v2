a9763d3164fef7d423afeff7eeb0545e0713c8f9
Viktor Alex <viktoralex@365.is>
2017-10-18 17:59:40 +0000

    Permit all on meal plan page

--- src/main/java/is/hi/hbv/do_or_diet/configuration/SecurityConfiguration.java
+++ src/main/java/is/hi/hbv/do_or_diet/configuration/SecurityConfiguration.java
@@ -52,7 +52,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
 			.antMatchers("/login").permitAll()
 			.antMatchers("/registration").permitAll()
 			.antMatchers("/recipe/**").permitAll()
-			.antMatchers("/mealplan/**").hasAuthority("ADMIN").anyRequest()
+			.antMatchers("/mealplan/**").permitAll().anyRequest()
 			.authenticated().and().csrf().disable()
 			.formLogin().loginPage("/login").failureUrl("/login?error=true").defaultSuccessUrl("/")
 			.usernameParameter("email").passwordParameter("password").and().logout()
